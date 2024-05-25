fbe8097d17d8857d7d6df49e92f9d3736f1e5e59
pipe1z <you@example.com>
2024-05-24 19:17:20 -0500

    Adición de funcionalidades de login en el frontend

--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion-usuario',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./identificacion-usuario.component.css']
 })
 export class IdentificacionUsuarioComponent {
+  fGroup: FormGroup = new FormGroup({});
 
+  constructor(
+    private fb: FormBuilder,
+
+
+  ) {
+
+
+  }
+
+  ngOnInit() {
+    this.ConstruirFormulario();
+  }
+
+  ConstruirFormulario() {
+    this.fGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required]]
+    });
+  }
+
+
+  IdentificarUsuario(){
+    if(this.fGroup.invalid) {
+      alert('Datos invalidos')
+    } else {
+      
+      alert('Identificando...');
+    }
+    
+  }
+
+  get obtenerFormGroup() {
+    return this.fGroup.controls;
+  }
 }
