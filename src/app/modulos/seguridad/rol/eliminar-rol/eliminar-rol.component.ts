5c37d3eceb1adb128bd88aafadfb2815927bc768
Jhonder18 <jhon.patino29550@ucaldas.edu.co>
2024-06-19 01:21:26 -0500

    Operaciones CRUD para el Rol en la interfaz grafica

--- src/app/modulos/seguridad/rol/eliminar-rol/eliminar-rol.component.ts
+++ src/app/modulos/seguridad/rol/eliminar-rol/eliminar-rol.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RolModel } from 'src/app/modelos/rol.model';
+import { RolService } from 'src/app/servicios/parametros/rol.service';
 
 @Component({
   selector: 'app-eliminar-rol',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./eliminar-rol.component.css']
 })
 export class EliminarRolComponent {
+  recordId: string = "";
+  nombre: String = '';
+  comentarios: String = '';
 
+  constructor(
+    private servicio: RolService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.recordId = this.route.snapshot.params["id"];
+  }
+
+  ngOnInit(): void {
+    this.BuscarRegistro();
+  }
+
+  BuscarRegistro(){
+    this.servicio.BuscarRegistro(this.recordId).subscribe({
+      next: (datos: RolModel) => {
+        this.recordId = datos._id!;
+        this.nombre = datos.nombre!;
+        this.comentarios = datos.comentarios!;
+      },
+      error: (err: any) => {
+        alert("El registro no existe.");
+      }
+    })
+  }
+
+  EliminarRegistro(){
+    this.servicio.EliminarRegistro(this.recordId).subscribe({
+      next: (data: any) => {
+        alert("Información eliminada correctamente.");
+        this.router.navigate(["/seguridad/rol-listar"]);
+      },
+      error: (err: any) => {
+        alert("Ha ocurrido un error eliminando el registro.");
+      }
+    })
+  }
 }
+
